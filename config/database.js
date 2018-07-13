7e45082b79b14b53cf1e701bff111ad6694f9184
Shailesh Kurdekar <skurdekar@gmail.com>
2018-07-13 01:00:30 -0400

    config/database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://shailesh:shailesh@ds135421.mlab.com:35421/sskdb'}
+    module.exports = {mongoURI: 'mongodb://shailesh:shailesh1@ds135421.mlab.com:35421/sskdb'}
 }else{
     module.exports = {mongoURI: 'mongodb://localhost:27017/vidjot-dev' }
 }
